57a7fdc5bdda18f98b08fa6c8332c745b01cf611
chrobacik <chrobacik@hotmail.com>
2017-11-13 12:01:54 +0100

    Xamarin Forms solution and projects created

---
+++ Device2Device-FileIO/Device2Device-FileIOPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Device2DeviceFileIO
+{
+    public partial class Device2Device_FileIOPage : ContentPage
+    {
+        public Device2Device_FileIOPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
