a17018b3432f1162b462f49770d34a0865285ec7
chrobacik <chrobacik@hotmail.com>
2017-12-07 11:46:47 +0100

    Extended QRCode to generate an ImageSource

--- Device2Device-FileIO.Core/Classes/QRCode.cs
+++ Device2Device-FileIO.Core/Classes/QRCode.cs
@@ -1,16 +1,39 @@
 ﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
 namespace Device2DeviceFileIO.Classes
 {
     public class QRCode
     {
-        
         public String Url { get; set; }
         public String FileName { get; set; }
         public DateTime ExpirationDate { get; set; }
         public byte[] Key { get; set; }
 
-        public QRCode()
+        public String GetData()
         {
+            return Url;
+        }
+
+        public ImageSource CreateImage(int width, int height, int margin)
+        {
+            // Create QR code builder
+            var writer = new ZXing.BarcodeWriter<byte[]>
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Width = width,
+                    Height = height,
+                    Margin = margin
+                }
+            };
+
+            // Write data to QR code
+            var barcode = writer.Write(GetData());
+
+            return ImageSource.FromStream(() => new MemoryStream(barcode));
         }
     }
 }
