a95e627677e6b94e1ef30218a2f7fad89d2f5399
chrobacik <chrobacik@hotmail.com>
2017-12-08 22:44:21 +0100

    Removed unused file picker code (Android)

--- Device2Device-FileIO.Android/MainActivity.cs
+++ Device2Device-FileIO.Android/MainActivity.cs
@@ -34,9 +34,7 @@ namespace Device2DeviceFileIO.Droid
 
             var appShareHandler = ((App)App.Current).ShareHandler as ShareHandler;
             appShareHandler.SetContext(this);
-
             appShareHandler.HandleShareIntent();
-
 
             MessagingCenter.Subscribe<FileOperation.UploadMessage>(this, FileOperation.UPLOAD, message => {
 
@@ -55,58 +53,10 @@ namespace Device2DeviceFileIO.Droid
             MessagingCenter.Unsubscribe<FileOperation.UploadMessage>(this, FileOperation.UPLOAD);
             MessagingCenter.Unsubscribe<FileOperation.DownloadMessage>(this, FileOperation.DOWNLOAD);
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
         {
             global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
-        {
-            base.OnActivityResult(requestCode, resultCode, intent);
-
-            if (requestCode == PickImageId && intent != null)
-            {
-                Console.WriteLine("Intent result: {0}", intent.Data.ToString());
-
-                if (resultCode == Result.Ok)
-                {
-                    PickImageTaskCompletionSource.SetResult(GetPathToImage(intent.Data));
-                }
-                else
-                {
-                    PickImageTaskCompletionSource.SetResult(null);
-                }
-            }
-        }
-
-        /// <summary>
-        /// https://stackoverflow.com/questions/45692514/get-path-of-gallery-image-xamarin/45725693#45725693
-        /// </summary>
-        /// <returns>The path to image</returns>
-        /// <param name="uri">URI of selected image</param>
-        protected String GetPathToImage(Android.Net.Uri uri)
-        {
-            string doc_id = "";
-            using (var c1 = ContentResolver.Query(uri, null, null, null, null))
-            {
-                c1.MoveToFirst();
-                string document_id = c1.GetString(0);
-                doc_id = document_id.Substring(document_id.LastIndexOf(":", StringComparison.InvariantCulture) + 1);
-            }
-
-            string path = null;
-
-            // The projection contains the columns we want to return in our query.
-            string selection = MediaStore.Images.Media.InterfaceConsts.Id + " =? ";
-            using (var cursor = ContentResolver.Query(MediaStore.Images.Media.ExternalContentUri, null, selection, new string[] { doc_id }, null))
-            {
-                if (cursor == null) return path;
-                var columnIndex = cursor.GetColumnIndexOrThrow(MediaStore.Images.Media.InterfaceConsts.Data);
-                cursor.MoveToFirst();
-                path = cursor.GetString(columnIndex);
-            }
-
-            return path;
         }
     }
 }
